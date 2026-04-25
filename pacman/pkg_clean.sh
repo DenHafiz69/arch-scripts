8a807023341562e9800a1c89889155f69fd541ca
Den Hafiz <den.m.hafiz@gmail.com>
2026-04-25 14:54:51 +0800

    feat: add interactive prompt to remove orphan packages via pacman -Rns

--- pacman/pkg_clean.sh
+++ pacman/pkg_clean.sh
@@ -7,3 +7,14 @@ sudo paccache -ruk0
 
 echo "Cleaning user cache..."
 rm -rf ~/.cache/*
+
+read -p "Do you want to clear orphan packages? (y/N) " clear_orphans
+if [[ "$clear_orphans" =~ ^[Yy]$ ]]; then
+    orphans=$(pacman -Qtdq)
+    if [ -n "$orphans" ]; then
+        echo "Removing orphan packages..."
+        sudo pacman -Rns $orphans
+    else
+        echo "No orphan packages found."
+    fi
+fi
